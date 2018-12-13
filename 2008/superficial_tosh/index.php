5bfd16a3646547d7c32b24b9a4adda91ca425708
Ben Jackson <ben.d.jackson@outlook.com>
2018-12-13 15:46:46 +0000

    2008 to 2012 complete

--- 2008/superficial_tosh/index.php
+++ 2008/superficial_tosh/index.php
@@ -2,14 +2,10 @@
 set_include_path(implode(PATH_SEPARATOR,Array('.','./includes','../includes','../../includes',get_include_path())));
 include_once('header.php'); ?>
 
-
-<table class="table_style">
-	<tr>
-		<td class="gallery_td_style_l">
-			<img src="01.jpg">
-		</td>
-	</tr>
-</table>
-
+<div class="row gallery">
+	<div class="col-lg-4 col-sm-6 align-self-center col-centered gallery_td_style_l">
+		<img src="01.jpg">
+	</div>
+</div>
 
 <?php include_once('footer.php'); ?>
