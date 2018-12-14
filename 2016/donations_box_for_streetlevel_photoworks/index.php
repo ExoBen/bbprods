a445aee4150e22afd92cfcb4dfbd1c4e8c773f81
Ben Jackson <ben.d.jackson@outlook.com>
2018-12-14 12:31:55 +0000

    2016 and 2017 complete - all done

--- 2016/donations_box_for_streetlevel_photoworks/index.php
+++ 2016/donations_box_for_streetlevel_photoworks/index.php
@@ -2,20 +2,16 @@
 set_include_path(implode(PATH_SEPARATOR,Array('.','./includes','../includes','../../includes',get_include_path())));
 include_once('header.php'); ?>
 
-
-<table class="table_style">
-	<tr>
-		<td class="gallery_td_style_p">
-			<img src="01.jpg">
-		</td>
-		<td class="gallery_td_style_p">
-			<img src="02.jpg">
-		</td>
-		<td class="gallery_td_style_l">
-			<img src="03.jpg">
-		</td>
-	</tr>
-</table>
-
+<div class="row gallery">
+	<div class="col-lg-4 col-sm-6 align-self-center col-centered gallery_td_style_p">
+		<img src="01.jpg">
+	</div>
+	<div class="col-lg-4 col-sm-6 align-self-center col-centered gallery_td_style_p">
+		<img src="02.jpg">
+	</div>
+	<div class="col-lg-4 col-sm-6 align-self-center col-centered gallery_td_style_l">
+		<img src="03.jpg">
+	</div>
+</div>
 
 <?php include_once('footer.php'); ?>
